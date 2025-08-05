2249f1ad5dfcfede05a6b2a931efa7a2079565b9
Dmitry <76906744+s1r1m1r1@users.noreply.github.com>
2025-08-05 23:17:29 +0200

    update

---
+++ lib/sha_red.dart
@@ -0,0 +1,23 @@
+library;
+
+export 'package:sha_red/src/models/todo_dto.dart';
+export 'package:sha_red/src/models/create_todo_dto.dart';
+export 'package:sha_red/src/models/update_todo_dto.dart';
+export 'package:sha_red/src/models/token_dto.dart';
+export 'package:sha_red/src/models/connection_stats_dto.dart';
+export 'package:sha_red/src/models/message_dto.dart';
+export 'package:sha_red/src/models/ws_event_dto.dart';
+export 'package:sha_red/src/models/letter_dto.dart';
+export 'package:sha_red/src/models/ws_config_dto.dart';
+
+export 'package:sha_red/src/ws_models/ws_to_server.dart';
+export 'package:sha_red/src/ws_models/ws_event_to_server.dart';
+export 'package:sha_red/src/ws_models/ws_event_from_server.dart';
+export 'package:sha_red/src/ws_models/ws_from_server.dart';
+export 'package:sha_red/src/ws_models/payloads.dart';
+export 'package:sha_red/src/payload/id_payload.dart';
+
+// export 'package:shared/src/models/room_dto.dart';
+// export 'package:shared/src/models/room_member_dto.dart';
+
+export 'package:sha_red/src/enum/role.dart';
