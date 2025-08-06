7c847243f49e978ab6609b75d0f88d0e88270878
Dmitry <76906744+s1r1m1r1@users.noreply.github.com>
2025-08-06 20:41:31 +0200

    fix library missed files

--- lib/sha_red.dart
+++ lib/sha_red.dart
@@ -1,23 +1,20 @@
 library;
 
-export 'package:sha_red/src/models/todo_dto.dart';
-export 'package:sha_red/src/models/create_todo_dto.dart';
-export 'package:sha_red/src/models/update_todo_dto.dart';
-export 'package:sha_red/src/models/token_dto.dart';
-export 'package:sha_red/src/models/connection_stats_dto.dart';
-export 'package:sha_red/src/models/message_dto.dart';
-export 'package:sha_red/src/models/ws_event_dto.dart';
-export 'package:sha_red/src/models/letter_dto.dart';
-export 'package:sha_red/src/models/ws_config_dto.dart';
+export 'src/enum/role.dart';
+export 'src/models/connection_stats_dto.dart';
+export 'src/models/create_todo_dto.dart';
+export 'src/models/letter_dto.dart';
+export 'src/models/message_dto.dart';
+export 'src/models/todo_dto.dart';
+export 'src/models/token_dto.dart';
+export 'src/models/update_todo_dto.dart';
+export 'src/models/ws_config_dto.dart';
 
-export 'package:sha_red/src/ws_models/ws_to_server.dart';
-export 'package:sha_red/src/ws_models/ws_event_to_server.dart';
-export 'package:sha_red/src/ws_models/ws_event_from_server.dart';
-export 'package:sha_red/src/ws_models/ws_from_server.dart';
-export 'package:sha_red/src/ws_models/payloads.dart';
-export 'package:sha_red/src/payload/id_payload.dart';
+// Export ws_models
+export 'src/ws_models/payloads.dart';
+export 'src/ws_models/ws_event_from_server.dart';
+export 'src/ws_models/ws_event_to_server.dart';
+export 'src/ws_models/ws_from_server.dart';
+export 'src/ws_models/ws_to_server.dart';
 
-// export 'package:shared/src/models/room_dto.dart';
-// export 'package:shared/src/models/room_member_dto.dart';
-
-export 'package:sha_red/src/enum/role.dart';
+// TODO: Export any libraries intended for clients of this package.
