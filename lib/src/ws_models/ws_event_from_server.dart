e3a7855a2ee51650563616e9091c6b6787332e8c
Dmitry <76906744+s1r1m1r1@users.noreply.github.com>
2025-08-09 10:30:55 +0200

    wip auth in web_socket

--- lib/src/ws_models/ws_event_from_server.dart
+++ lib/src/ws_models/ws_event_from_server.dart
@@ -1,1 +1,17 @@
-enum WsEventFromServer { letters, counter, letterCreated, joinedCounter, joinedLetters, adminInfo }
+enum WsEventFromServer {
+  // return new tokens
+  loggedIn,
+  // ask update with refresh token to get new tokens
+  tokenExpired,
+  refreshTokenExpired,
+  // log out , unknown user
+  // list of online users
+  onlineUsers,
+  unauthenticated,
+  letters,
+  counter,
+  letterCreated,
+  joinedCounter,
+  joinedLetters,
+  adminInfo,
+}
